e4d7510bf8056fcdd01f7f16c90bd4e29480e55c
Claudiu <claudiu.lintes@student.upt.ro>
2024-03-24 23:15:57 +0200

    Version 3

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This version 2");
         int x=3;
+        int y=5;
+        System.out.println(x+y);
     }
 }
